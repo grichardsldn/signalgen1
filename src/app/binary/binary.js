208345787b7eed44d3b29e1ead51d7e6557bc057
Graham Richards <grahamdrichards@gmail.com>
2015-02-13 17:43:43 +0000

    More UI, has a problem with the textarea at the moment

--- src/app/binary/binary.js
+++ src/app/binary/binary.js
@@ -22,19 +22,19 @@ angular.module( 'ngBoilerplate.binary', [
 .controller( 'BinaryCtrl', function BinaryCtrl( $scope, playerService, binarySynth ) {
   
   $scope.defaults = function() {
-    $scope.param_text = "Payload goes here";
-    $scope.param_period = "30";
-    $scope.param_start_bits = "0";
-    $scope.param_stop_bits = "0";
-    $scope.param_frame_gap = 20;
-    $scope.param_repeat_count = 0;
-    $scope.param_sevenbit = 1;
-    $scope.param_big_endian = 0 ;
-    $scope.param_parity = 0 ;
-    $scope.param_checksum = 0;
-    $scope.param_mtu = 1500;
-    $scope.param_encoding = "NRZ";
-    $scope.param_encryption = "1";
+    $scope.param_text = 'Payload goes here';
+    $scope.param_period = '30';
+    $scope.param_start_bits = '0';
+    $scope.param_stop_bits = '0';
+    $scope.param_frame_gap = '20';
+    $scope.param_repeat_count = '0';
+    $scope.param_sevenbit = '1';
+    $scope.param_big_endian = '1';
+    $scope.param_parity = '0';
+    $scope.param_checksum = '0';
+    $scope.param_mtu = '1500';
+    $scope.param_encoding = 'NRZ';
+    $scope.param_encryption = "0";
   }; 
 
   $scope.defaults();
@@ -63,15 +63,15 @@ angular.module( 'ngBoilerplate.binary', [
     var params = {
       text: $scope.param_text,
       period: parseInt($scope.param_period, 10 ),
-      start_bits: $scope.param_start_bits,
-      stop_bits: $scope.param_stop_bits,
-      frame_gap: $scope.param_frame_gap,
-      repeat_count: $scope.param_repeat_count,
-      sevenbit: $scope.param_sevenbit,
-      big_endian: $scope.param_big_endian,
-      parity: $scope.param_parity,
-      checksum: $scope.param_checksum,
-      mtu: $scope.param_mtu,
+      start_bits: parseInt($scope.param_start_bits, 10),
+      stop_bits: parseInt($scope.param_stop_bits, 10),
+      frame_gap: parseInt($scope.param_frame_gap, 10),
+      repeat_count: parseInt($scope.param_repeat_count, 10),
+      sevenbit: parseInt($scope.param_sevenbit, 10),
+      big_endian: parseInt($scope.param_big_endian, 10),
+      parity: parseInt($scope.param_parity, 10),
+      checksum: parseInt($scope.param_checksum, 10),
+      mtu: parseInt($scope.param_mtu, 10),
       encoding: $scope.param_encoding,
       encryption: parseInt($scope.param_encryption, 10 )
     };
